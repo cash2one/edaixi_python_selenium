537faac48257fe22c735e7cc46cdbb9ccd69e640
ibmcuijunluke <ibmcuijun2015@126.com>
2015-08-14 15:44:16 +0800

    newest

---
+++ src/uibusinesstestcase/apptaskcaiwu.py
@@ -0,0 +1,5 @@
+'''
+Created on 2015年8月14日
+
+@author: edaixicuijun
+'''
---
+++ src/uibusinesstestcase/apptaskops.py
@@ -0,0 +1,7 @@
+#-*-coding:utf-8-*- 
+#coding=utf-8
+from uiappobject import appobjectops
+
+class apptaskopssystem:
+    
+    def getLoginOpsSystem(self):
