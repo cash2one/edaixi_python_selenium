13985fdf21e6ac6cc6b57679879ad33d5bd88f17
ibmcuijunluke <ibmcuijun2015@126.com>
2015-08-14 11:35:15 +0800

    updated by 201508newest

--- src/uiappobject/appobjectops.py
+++ src/uiappobject/appobjectops.py
@@ -4,6 +4,19 @@ from appobjectutils import SingleWebDriver
 #class appobjectops:
 # class appObjectUtils(self):
 #     
-def getWebElementOpsRechargereturncrash(self):
+class appobjectops:
+    loginClickButton = "div#container.container h3.text-center.text-primary a.btn.btn-success.text-center"
+    clickRechargeReturncrashLink= "div.container>div.navbar-collapse.collapse.navbar-responsive-collapse>ul.nav.navbar-nav>li:nth-child("+str(8)+") a"
+    
+    clickNewButtonRechargeReturncrash="div#container.container div a.btn.btn-info.btn-info"
+    addchizhifanxianResult="div#container.container div.alert.fade.in.alert-success"
+    
+    clickEditButtonRechargeReturncrash="div#container.container table.table.table-bordered.table-striped tbody tr:nth-child(2) td:last-child a:first-child"
+    editchizhifanxianresult="div#container.container div.alert.fade.in.alert-success"
+    
+    clickDeleteButtonRechargeReturncrash="div#container.container table.table.table-bordered.table-striped tbody tr:nth-child(2) td:last-child a:last-child"
+    deletechizhifanxianresult="div#container.container div.alert.fade.in.alert-success"
+    
+    def getWebElementOpsRechargereturncrash(self):
         #SingleWebDriver.getWebDriverInstance().find_element_by_css_selector("div.container>div.navbar-collapse.collapse.navbar-responsive-collapse>ul.nav.navbar-nav>li:nth-child("+str(8)+") a").click()
         return SingleWebDriver.getWebDriverInstance(self).find_element_by_css_selector("div.container>div.navbar-collapse.collapse.navbar-responsive-collapse>ul.nav.navbar-nav>li:nth-child("+str(8)+") a")
--- src/uiopstestcase/ops_testcase08_recharge_returncrash.py
+++ src/uiopstestcase/ops_testcase08_recharge_returncrash.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re ,ConfigParser
-from uiappobject.appobjectutils import SingleWebDriver
+from uiappobject.appobjectops import appobjectops
 
 class OpsTestcase08rechargereturncrash(unittest.TestCase):
     def setUp(self):
@@ -28,7 +28,7 @@ class OpsTestcase08rechargereturncrash(unittest.TestCase):
     def test_ops_testcase08_rechargereturncrash(self):
         driver = self.driver
         driver.get(self.base_url + "/")
-        driver.find_element_by_css_selector("div#container.container h3.text-center.text-primary a.btn.btn-success.text-center").click()
+        driver.find_element_by_css_selector(appobjectops.loginClickButton).click()
         driver.find_element_by_id("username").clear()
         driver.find_element_by_id("username").send_keys(USER_NAME)
         driver.find_element_by_id("password").clear()
@@ -37,13 +37,13 @@ class OpsTestcase08rechargereturncrash(unittest.TestCase):
         print driver.title
         self.assertEqual(driver.title, u"e袋洗城市运营后台")
         
-        driver.find_element_by_css_selector("div.container>div.navbar-collapse.collapse.navbar-responsive-collapse>ul.nav.navbar-nav>li:nth-child("+str(8)+") a").click()
+        #driver.find_element_by_css_selector("div.container>div.navbar-collapse.collapse.navbar-responsive-collapse>ul.nav.navbar-nav>li:nth-child("+str(8)+") a").click()
         #getWebElementOpsRechargereturncrash(driver).click()
-        
+        driver.find_element_by_css_selector(appobjectops.clickRechargeReturncrashLink).click()
         driver.implicitly_wait(10)
         print driver.title
-        driver.find_element_by_css_selector("div#container.container div a.btn.btn-info.btn-info").send_keys(Keys.ENTER)
-        
+        #driver.find_element_by_css_selector("div#container.container div a.btn.btn-info.btn-info").send_keys(Keys.ENTER)
+        driver.find_element_by_css_selector(appobjectops.clickNewButtonRechargeReturncrash).send_keys(Keys.ENTER)
         #html body div#container.container div a.btn.btn-info.btn-info
         self.assertEqual(driver.title, u"e袋洗城市运营后台")
         #driver.find_element_by_link_text(u"新 建").click()
@@ -55,31 +55,36 @@ class OpsTestcase08rechargereturncrash(unittest.TestCase):
 #         driver.find_element_by_id("recharge_setting_form_max").send_keys("1000")
         driver.find_element_by_name("commit").click()
         self.assertEqual(driver.title, u"e袋洗城市运营后台")
-        addchizhifanxian=driver.find_element_by_css_selector("div#container.container div.alert.fade.in.alert-success").text
+        #addchizhifanxian=driver.find_element_by_css_selector("div#container.container div.alert.fade.in.alert-success").text
+        addchizhifanxian=driver.find_element_by_css_selector(appobjectops.addchizhifanxianResult).text
         print addchizhifanxian
         assert u"返现设置已添加"  in (addchizhifanxian)
         #self.assertEqual(addchizhifanxian, u"返现设置已添加")
         
-        driver.find_element_by_css_selector("div#container.container table.table.table-bordered.table-striped tbody tr:nth-child(2) td:last-child a:first-child").send_keys(Keys.ENTER)
+        #driver.find_element_by_css_selector("div#container.container table.table.table-bordered.table-striped tbody tr:nth-child(2) td:last-child a:first-child").send_keys(Keys.ENTER)
+        driver.find_element_by_css_selector(appobjectops.clickEditButtonRechargeReturncrash).click()
         #driver.find_element_by_link_text(u"编辑").click()
         self.assertEqual(driver.title, u"e袋洗城市运营后台")
         driver.find_element_by_id("recharge_setting_form_min").clear()
         driver.find_element_by_id("recharge_setting_form_min").send_keys("200")
         driver.find_element_by_name("commit").click()
         #html body div#container.container div.alert.fade.in.alert-success
-        editchizhifanxian=driver.find_element_by_css_selector("div#container.container div.alert.fade.in.alert-success").text
+        #editchizhifanxian=driver.find_element_by_css_selector("div#container.container div.alert.fade.in.alert-success").text
+        editchizhifanxian=driver.find_element_by_css_selector(appobjectops.editchizhifanxianresult).text
         print editchizhifanxian
         assert u"返现设置已更新" in (editchizhifanxian)
         #self.assertEqual(editchizhifanxian, u"返现设置已更新")
         
         #html body div#container.container table.table.table-bordered.table-striped tbody tr:nth-child(2) td:last-child a:last-child.btn.btn-sm.btn-danger
-        driver.find_element_by_css_selector("div#container.container table.table.table-bordered.table-striped tbody tr:nth-child(2) td:last-child a:last-child").send_keys(Keys.ENTER)
+        #driver.find_element_by_css_selector("div#container.container table.table.table-bordered.table-striped tbody tr:nth-child(2) td:last-child a:last-child").send_keys(Keys.ENTER)
         #driver.find_element_by_link_text(u"删除").click()
+        driver.find_element_by_css_selector(appobjectops.clickDeleteButtonRechargeReturncrash).click()
         time.sleep(2)
         self.assertEqual(u"确认删除", self.close_alert_and_get_its_text())
         #driver.find_element_by_link_text(u"城市管理").click()
         #html body div#container.container
-        deletechizhifanxian=driver.find_element_by_css_selector("div#container.container div.alert.fade.in.alert-success").text
+        #deletechizhifanxian=driver.find_element_by_css_selector("div#container.container div.alert.fade.in.alert-success").text
+        deletechizhifanxian=driver.find_element_by_css_selector(appobjectops.deletechizhifanxianresult).text
         print deletechizhifanxian
         assert u"返现设置删除成功" in (deletechizhifanxian)
         #self.assertEqual(deletechizhifanxian, u"返现设置删除成功")
