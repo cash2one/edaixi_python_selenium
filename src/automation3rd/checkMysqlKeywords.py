f1a1fbc5d43135157ffc8df86bae12f37b027618
ibmcuijunluke <ibmcuijun2015@126.com>
2015-08-27 16:12:21 +0800

    submitted new changed for 3rd automation

--- src/automation3rd/checkMysqlKeywords.py
+++ src/automation3rd/checkMysqlKeywords.py
@@ -10,7 +10,7 @@ class checkMysqlKeywordsClass(unittest.TestCase):
     def setUp(self):
         global dict_data,conn,cursor
         dict_data = {}
-        with open('C://edaixi_testdata//rubykeyword.txt', 'r') as df:
+        with open('C://edaixi_testdata//mysqlkeyword.txt', 'r') as df:
             for kv in [d.strip().split(' ') for d in df]:
                    dict_data[kv[0]] = kv[1]
 
@@ -30,29 +30,41 @@ class checkMysqlKeywordsClass(unittest.TestCase):
         global cloumnname
         cursor = conn.cursor()
         table_ims_washing_order="ims_washing_order"
-        n = cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name LIKE '"+table_ims_washing_order+"';")
-        for row in cursor.fetchall():
-        #print " the row is ",str(row)[3:-3]
-            cloumnname=str(row)[3:-3]
-            for k in dict_data:
-                #print " Mysql Keywords dict_data[k] file dict is ",dict_data[k]
-                #self.assertEqual(dict_data[k], cloumnname)
-                if dict_data[k]==cloumnname:
-                   print "the table",table_ims_washing_order," has column name ",cloumnname," is exsit on ruby baoliiu keyword"
-                   raise ValueError(" the table has column name exsit on MYSQL Table keywords")
-                   
-                else:
-                   print ""
-                   pass
+#         n = cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name LIKE '"+table_ims_washing_order+"';")
+#         for row in cursor.fetchall():
+#         #print " the row is ",str(row)[3:-3]
+#             cloumnname=str(row)[3:-3]
+#             for k in dict_data:
+#                 #print " Mysql Keywords dict_data[k] file dict is ",dict_data[k]
+#                 #self.assertEqual(dict_data[k], cloumnname)
+#                 if dict_data[k]==cloumnname:
+#                    print "the table",table_ims_washing_order," has column name ",cloumnname," is exsit on ruby baoliiu keyword"
+#                    raise ValueError(" the table has column name exsit on MYSQL Table keywords")
+#                    
+#                 else:
+#                    print ""
+#                    pass
     
         databasename="rongchain04"
         n = cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='"+databasename+"';")
         for row in cursor.fetchall():
             #print str(row)
             tablename=str(row)[3:-3]
-            print " test 05 datatbase rongchain04",tablename
+            #print " test 05 datatbase rongchain04",tablename
 
-
+            n = cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name LIKE '"+tablename+"';")
+            for row in cursor.fetchall():
+            #print " the row is ",str(row)[3:-3]
+                cloumnname=str(row)[3:-3]
+                for k in dict_data:
+                #print " Mysql Keywords dict_data[k] file dict is ",dict_data[k]
+                #self.assertEqual(dict_data[k], cloumnname)
+                   if dict_data[k]==cloumnname:
+                      print "the table",tablename," has column name ",cloumnname," is exsit on mysql revise keyword"
+                      raise ValueError(" the table has column name exsit on MYSQL Table keywords")
+                   else:
+                      print ""
+                      pass
            
            
 if __name__ == "__main__":
